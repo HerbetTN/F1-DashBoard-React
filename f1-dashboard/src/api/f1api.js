b9e52e7c6acec0d875d70f4f83b801a2cf3b9ea5
TapiwaHerbetNosenga <144127869+TapiwaHerbetNosenga@users.noreply.github.com>
2026-08-31 23:36:27 +0300

    React router implemented, App.jsx has been moved to HomePage.jsx

--- f1-dashboard/src/api/f1api.js
+++ f1-dashboard/src/api/f1api.js
@@ -3,3 +3,9 @@ export async function getDriverStandings() {
   const data = await res.json();
   return data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
 }
+
+export async function getSeasonWinners(year) {
+  const res = await fetch(`https://api.jolpi.ca/ergast/f1/${year}/results/1.json`);
+  const data = await res.json();
+  return data.MRData.RaceTable.Races;
+}
